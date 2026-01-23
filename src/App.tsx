b71a69138d25c44c38ff4721cc3afb5d43c9d87c
ponesicek <62983557+Ponesicek@users.noreply.github.com>
2026-01-23 20:50:07 +0100

    sidebar v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
+import { Field, FieldDescription, FieldLabel } from "@/components/ui/field";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "./components/ui/button";
+import { Slider } from "./components/ui/slider";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold text-gray-800">💖 Hello World!</h1>
-        <p className="mt-4 text-gray-600">
-          Welcome to your Electron application.
-        </p>
+    <div className="h-screen w-full bg-gray-100 flex p-4">
+      <div className="flex gap-2 flex-col items-center min-w-sm">
+        <h2 className="text-xl">How are we feeling?</h2>
+        <Textarea id="textarea-message" placeholder="Type your message here." />
+        <div className="flex flex-row w-full justify-end">
+          <Button>Tune in</Button>
+        </div>
+        <div className="flex flex-row gap-2 w-full">
+          <span className="min-w-fit">News frequency: </span>{" "}
+          <Slider className="w-full " />
+        </div>
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium w-full">Metal</TableCell>
+              <TableCell>
+                <button>X</button>
+              </TableCell>
+              <TableCell className="text-right">
+                <button>➡️</button>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
       </div>
     </div>
   );
