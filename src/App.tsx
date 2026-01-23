6fb90f7f5ab90e054e5d3c660747a242670b3fb2
ponesicek <62983557+Ponesicek@users.noreply.github.com>
2026-01-23 20:58:14 +0100

    sidebar v2

--- src/App.tsx
+++ src/App.tsx
@@ -11,33 +11,61 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { X, ArrowRight } from "lucide-react";
+import { Separator } from "./components/ui/separator";
+
 export default function App() {
   return (
-    <div className="h-screen w-full bg-gray-100 flex p-4">
+    <div className="h-screen w-full bg-gray-100 flex p-4 gap-4">
       <div className="flex gap-2 flex-col items-center min-w-sm">
         <h2 className="text-xl">How are we feeling?</h2>
-        <Textarea id="textarea-message" placeholder="Type your message here." />
+        <Textarea
+          id="textarea-message"
+          placeholder="Type your message here."
+          className="select-none"
+        />
         <div className="flex flex-row w-full justify-end">
           <Button>Tune in</Button>
         </div>
         <div className="flex flex-row gap-2 w-full">
           <span className="min-w-fit">News frequency: </span>{" "}
-          <Slider className="w-full " />
+          <Slider
+            className="w-full "
+            min={0}
+            max={4}
+            step={1}
+            defaultValue={[0]}
+          />
         </div>
         <Table>
           <TableBody>
-            <TableRow>
-              <TableCell className="font-medium w-full">Metal</TableCell>
-              <TableCell>
-                <button>X</button>
-              </TableCell>
-              <TableCell className="text-right">
-                <button>➡️</button>
-              </TableCell>
-            </TableRow>
+            <TableRowComponent name="Metal" />
+            <TableRowComponent name="Metal" />
+            <TableRowComponent name="Metal" />
+            <TableRowComponent name="Metal" />
+            <TableRowComponent name="Metal" />
           </TableBody>
         </Table>
       </div>
+      <Separator orientation="vertical" />
     </div>
+  );
+}
+
+function TableRowComponent({ name }: { name: string }) {
+  return (
+    <TableRow>
+      <TableCell className="font-medium w-full">{name}</TableCell>
+      <TableCell>
+        <button>
+          <X size={16} />
+        </button>
+      </TableCell>
+      <TableCell className="text-right">
+        <button>
+          <ArrowRight size={16} />
+        </button>
+      </TableCell>
+    </TableRow>
   );
 }
